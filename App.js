17de0c53fe902e41ac2a03f852987efcdf58e609
Josué Makuta <makutajosue@gmail.com>
2023-01-06 19:37:58 +0100

    Text component

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 // import { StatusBar } from "expo-status-bar";
-import { StyleSheet,  Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-web";
 
 // import { TextInput } from "react-native-web";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello world ...</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text
+        numberOfLines={1}
+        onPress={() => {
+          console.log("The text is pressed ...");
+        }}
+      >
+        If this text is just longer than the number of lines provided, it will
+        get trunked and this prop will add ... at the end of the line.
+      </Text>
+    </View>
   );
 }
 
